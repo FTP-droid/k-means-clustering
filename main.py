8f7f6e18ceb62c881624e15c0f53188401fadcaf
Alaister <77394050+FTP-droid@users.noreply.github.com>
2023-07-19 23:09:57 -0400

    Added line for printing the cluster centers, migrated code for plotting the X data to prepare the plot to display the cluster centroids

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ X1 = 1 + 2 * np.random.rand(50, 2)
 # X from rows 49 to 99 and both columns replaced by the values from X1 which have values in the space (1, 3)
 X[50:100, :] = X1
 
-# plot the X values with x values consisting of the first column of rows in X and y values consisting of
-# the second column of rows in X. Marker size set to 50 & color of points set to blue.
-plt.scatter(X[:, 0], X[:, 1], s=50, c='b')
-plt.show()
-
 # Set up a KMeans clustering algorithm with number of clusters = 2, init = 10 resulting in 10 different
 # KMeans clustering algorithm runs. Finally fit the data in X using the defined Kmeans algorithm.
 Kmean = KMeans(n_clusters=2)
 Kmean.fit(X)
+
+# Prints the center of the clusters of X
+print(Kmean.cluster_centers_)
+
+
+# plot the X values with x values consisting of the first column of rows in X and y values consisting of
+# the second column of rows in X. Marker size set to 50 & color of points set to blue.
+plt.scatter(X[:, 0], X[:, 1], s=50, c='b')
+plt.show()
