1ec028e6187578d82000c2067929591ca0f29f55
Alaister <77394050+FTP-droid@users.noreply.github.com>
2023-07-19 22:45:58 -0400

    Changed 3rd comment to better reflect what values are mutated in X. Created a scatter plot using matplotlib.pyplot and graphed the scatter plot.

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ X = -2 * np.random.rand(100, 2)
 # X1 will be a 2 dim array with values (1, 3) across 50 rows and 2 columns
 X1 = 1 + 2 * np.random.rand(50, 2)
 
-# X from rows 49 to 99 replaced by the values from X1 which have values in the space (1, 3)
+# X from rows 49 to 99 and both columns replaced by the values from X1 which have values in the space (1, 3)
 X[50:100, :] = X1
+
+# plot the X values with x values consisting of the first column of rows in X and y values consisting of
+# the second column of rows in X. Marker size set to 50 & color of points set to blue.
+plt.scatter(X[:, 0], X[:, 1], s=50, c='b')
+plt.show()
